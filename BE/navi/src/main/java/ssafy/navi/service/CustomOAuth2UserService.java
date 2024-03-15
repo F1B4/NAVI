32d8b02259b101e0912a23a0f2f0f08e69cf47b1
suyeon-hyeon <shonee99@naver.com>
2024-03-15 16:50:20 +0900

    fix : 시큐리티 진행상황 갱신

---
+++ BE/navi/src/main/java/ssafy/navi/service/CustomOAuth2UserService.java
@@ -0,0 +1,12 @@
+package ssafy.navi.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+}
