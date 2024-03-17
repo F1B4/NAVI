5f0af14992fe53544c9b894a89f97da95df9402b
suyeon-hyeon <shonee99@naver.com>
2024-03-18 02:38:21 +0900

    fix : 소셜 로그인 요청까지 완료

--- BE/navi/src/main/java/ssafy/navi/config/SecurityConfig.java
+++ BE/navi/src/main/java/ssafy/navi/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package ssafy.navi.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import ssafy.navi.service.CustomOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,12 +29,14 @@ public class SecurityConfig {
                 .httpBasic((basic) -> basic.disable());
 
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService)));
 
-//        http
-//                .authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
-//                        .anyRequest().authenticated());
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .anyRequest().authenticated());
 
         return http.build();
     }
--- BE/navi/src/main/java/ssafy/navi/service/CustomOAuth2UserService.java
+++ BE/navi/src/main/java/ssafy/navi/service/CustomOAuth2UserService.java
@@ -3,10 +3,40 @@ package ssafy.navi.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import ssafy.navi.dto.CustomOAuth2User;
+import ssafy.navi.dto.GoogleResponse;
+import ssafy.navi.dto.NaverResponse;
+import ssafy.navi.dto.OAuth2Response;
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    // DefaultOAuth2UserService OAuth2UserService의 구현체
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println(oAuth2User.getAttributes());
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("naver")) {
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        else if (registrationId.equals("google")) {
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        }
+        else {
+            return null;
+        }
+
+        String role = "ROLE_USER";
+
+        return new CustomOAuth2User(oAuth2Response, role);
+    }
 }
