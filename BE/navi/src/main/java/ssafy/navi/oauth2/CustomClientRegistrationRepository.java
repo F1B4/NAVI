1e4eece549682d770c550452f2f25101ad24f0ab
suyeon-hyeon <shonee99@naver.com>
2024-03-22 09:36:02 +0900

    fix : 변수 설정 주석 추가

--- BE/navi/src/main/java/ssafy/navi/oauth2/CustomClientRegistrationRepository.java
+++ BE/navi/src/main/java/ssafy/navi/oauth2/CustomClientRegistrationRepository.java
@@ -5,11 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
+/*
+ClientRegistration의 저장소
+서비스별 ClientRegistration들을 가짐
+ */
 @Configuration
 @RequiredArgsConstructor
 public class CustomClientRegistrationRepository {
     private final SocialClientRegistration socialClientRegistration;
 
+    // InMemoryClientRegistrationRepository 클래스를 사용하여 인메모리 방식으로 관리를 진행
     public ClientRegistrationRepository clientRegistrationRepository() {
         return new InMemoryClientRegistrationRepository(socialClientRegistration.naverClientRegistration(), socialClientRegistration.googleClientRegistration());
     }
