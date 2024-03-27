8d420c8f320b13c81dac30f5ab4a7aceea4e11e9
LEESEOM <dltjdah25@naver.com>
2024-03-27 17:32:28 +0900

    fix: 유저 상태 관리, 메인 페이지 체크

--- FE/navi/src/widgets/LayoutSidebar/ui/LayoutSidebar/LayoutSidebar.tsx
+++ FE/navi/src/widgets/LayoutSidebar/ui/LayoutSidebar/LayoutSidebar.tsx
@@ -1,12 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Btn } from '@/shared/ui';
 import css from './LayoutSidebar.module.css';
 
 export function LayoutSidebar() {
   return (
     <div className={css.root}>
-      <Link to={'/'}>홈</Link>
-      <Link to={'/cover'}>커버</Link>
-      <Link to={'/noraebang'}>노래방</Link>
+      <Btn
+        to="/"
+        className={css.btn}
+        icon="images/home_icon.png"
+        content="홈"
+      ></Btn>
+      <Btn
+        to="/cover"
+        className={css.btn}
+        icon="images/cover_icon.png"
+        content="커버"
+      ></Btn>
+      <Btn
+        to="/noraebang"
+        className={css.btn}
+        icon="images/noraebang_icon.png"
+        content="노래방"
+      ></Btn>
+      <hr />
     </div>
   );
 }
