69db9f4785b620aa1626ed3088f74ca6865b2b58
황윤정 <dbswjd3828@gmail.com>
2024-03-29 17:15:33 +0900

    feat: ai커버 생성 후 영상처리 완료

--- BE/navi-ai/server.py
+++ BE/navi-ai/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from server_package.train_process import train_model_task
 from server_package.infer_process import infer_task
 
@@ -6,11 +6,14 @@ app = FastAPI()
 
 # AI 모델 학습 요청 엔드포인트
 @app.post("/ai/train")
-async def train(user_pk : int):
-    train_model_task.delay(user_pk)
+async def train(pk : str = Form(...)):
+    print("연결 완료")
+    train_model_task.delay(pk)
 
 
 # 커버곡 생성(추론 요청) 엔드포인트
 @app.post("/ai/cover")
 async def infer(cover_pk : int):
     infer_task.delay(cover_pk)
+
+# mr+유저 목소리 합쳐서 s3 올리고 노래방테이블의 record에 s3 주소 저장 
