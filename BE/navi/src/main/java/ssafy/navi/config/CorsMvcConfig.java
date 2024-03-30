4d0fe54b50988fdc00ee05bba453d0c6f0a8e783
유형민 <hmy940424@naver.com>
2024-03-30 17:55:07 +0900

    fix: react bug

--- BE/navi/src/main/java/ssafy/navi/config/CorsMvcConfig.java
+++ BE/navi/src/main/java/ssafy/navi/config/CorsMvcConfig.java
@@ -12,8 +12,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
-                .allowedOrigins("http://localhost:5173");
-//                .allowedOrigins("https://j10d107.p.ssafy.io");
+//                .allowedOrigins("http://localhost:5173");
+                .allowedOrigins("https://j10d107.p.ssafy.io");
         //서버용
     }
 }
