5de3db442199c16933ef0fb38933b12ec8a1943a
유형민 <hmy940424@naver.com>
2024-03-30 17:51:55 +0900

    feat: 서버, 로컬 구분 주석

--- FE/navi/src/widgets/LayoutHeader/ui/Login/Login.tsx
+++ FE/navi/src/widgets/LayoutHeader/ui/Login/Login.tsx
@@ -2,16 +2,21 @@ import { useUserStore } from '@/shared/store';
 import css from './Login.module.css';
 
 const onNaverLogin = () => {
-  window.location.href = 'http://localhost:8081/api/oauth2/authorization/naver';
+  window.location.href =
+    // 'https://j10d107.p.ssafy.io/api/oauth2/authorization/naver';
+    'http://localhost:8081/api/oauth2/authorization/naver';
 };
 
 const onGoogleLogin = () => {
   window.location.href =
+    // 'https://j10d107.p.ssafy.io/api/oauth2/authorization/google';
     'http://localhost:8081/api/oauth2/authorization/google';
 };
 
 const onKakaoLogin = () => {
-  window.location.href = 'http://localhost:8081/api/oauth2/authorization/kakao';
+  window.location.href =
+    // 'https://j10d107.p.ssafy.io/api/oauth2/authorization/kakao';
+    'http://localhost:8081/api/oauth2/authorization/kakao';
 };
 
 export function Login() {
