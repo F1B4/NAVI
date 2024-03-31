0b1ce99e6e580151a49d308cea06c782c8ee8eba
유형민 <hmy940424@naver.com>
2024-03-31 20:56:34 +0900

    fix: config

--- FE/navi/vite.config.ts
+++ FE/navi/vite.config.ts
@@ -10,11 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    port: 5173,
-    strictPort: false,
-    hmr: {
-      port: 5173,
-    },
-  },
+  // server: {
+  //   port: 5173,
+  //   strictPort: false,
+  //   hmr: {
+  //     port: 5173,
+  //   },
+  // },
 });
