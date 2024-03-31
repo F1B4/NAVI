4a251c075fcaeace57a9b73b40e8f682621d83dc
황윤정 <dbswjd3828@gmail.com>
2024-04-01 02:02:48 +0900

    fix: baseAPI

--- FE/navi/src/shared/url/baseUrl.ts
+++ FE/navi/src/shared/url/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:5173/';
-// export const baseUrl = 'https://j10d107.p.ssafy.io/';
+// export const baseUrl = 'http://localhost:5173/';
+export const baseUrl = 'https://j10d107.p.ssafy.io/';
--- FE/navi/src/widgets/LayoutHeader/ui/Login/Login.tsx
+++ FE/navi/src/widgets/LayoutHeader/ui/Login/Login.tsx
@@ -3,20 +3,20 @@ import css from './Login.module.css';
 
 const onNaverLogin = () => {
   window.location.href =
-    // 'https://j10d107.p.ssafy.io/api/oauth2/authorization/naver';
-    'http://localhost:8081/api/oauth2/authorization/naver';
+    'https://j10d107.p.ssafy.io/api/oauth2/authorization/naver';
+  // 'http://localhost:8081/api/oauth2/authorization/naver';
 };
 
 const onGoogleLogin = () => {
   window.location.href =
-    // 'https://j10d107.p.ssafy.io/api/oauth2/authorization/google';
-    'http://localhost:8081/api/oauth2/authorization/google';
+    'https://j10d107.p.ssafy.io/api/oauth2/authorization/google';
+  // 'http://localhost:8081/api/oauth2/authorization/google';
 };
 
 const onKakaoLogin = () => {
   window.location.href =
-    // 'https://j10d107.p.ssafy.io/api/oauth2/authorization/kakao';
-    'http://localhost:8081/api/oauth2/authorization/kakao';
+    'https://j10d107.p.ssafy.io/api/oauth2/authorization/kakao';
+  // 'http://localhost:8081/api/oauth2/authorization/kakao';
 };
 
 export function Login() {
