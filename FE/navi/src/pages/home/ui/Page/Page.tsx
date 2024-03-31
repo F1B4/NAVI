b14001dabfadf16118b4709e3d2991ba91ef3e8e
유형민 <hmy940424@naver.com>
2024-03-31 17:20:06 +0900

    fix: websocket

--- FE/navi/src/pages/home/ui/Page/Page.tsx
+++ FE/navi/src/pages/home/ui/Page/Page.tsx
@@ -13,8 +13,8 @@ export function HomePage() {
   const params = new URLSearchParams(search);
   const loginSuccess = params.get('loginSuccess');
 
-  const noti = () => {
-    axios.get(`${baseApi}/sse/notification/subscribe/${store.userId}`, {
+  const noti = async () => {
+    await axios.get(`${baseApi}/sse/notification/subscribe/${store.userId}`, {
       headers: {
         Accept: 'text/event-stream',
         'Cache-Control': 'no-cache',
@@ -26,7 +26,7 @@ export function HomePage() {
     const fetchData = async () => {
       if (loginSuccess && store.userId === 0) {
         store.getData();
-        noti();
+        await noti();
       }
     };
     fetchData();
