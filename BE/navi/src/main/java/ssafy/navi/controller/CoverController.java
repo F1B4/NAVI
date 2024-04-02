eb74023ec552f370b8867c075cecf57f381a298a
LEESEOM <dltjdah25@naver.com>
2024-04-02 17:29:09 +0900

    merge: dev

--- BE/navi/src/main/java/ssafy/navi/controller/CoverController.java
+++ BE/navi/src/main/java/ssafy/navi/controller/CoverController.java
@@ -91,16 +91,23 @@ public class CoverController {
     /*
     매칭 요청하기
      */
-    @PostMapping("")
+    @PostMapping("/create")
     public Response<?> createCover(@RequestBody CoverRegistDto coverRegistDto) throws Exception{
         Response<Long> result = coverService.createCover(coverRegistDto);
         if (result.getMessage().equals("1")) {
-            System.out.println("result.getData() @@@@@@@@@@@@@ " + result.getData());
-            fastApiService.fetchDataFromFastAPI("/ai/cover", result.getData());
-        } else if (result.getMessage() == "2") {
-            fastApiService.fetchDataFromFastAPI("/ai/cover", result.getData());
-        }
 
+                System.out.println(result.getData() + "@@@11111111111111111111111111111111111");
+                fastApiService.fetchDataFromFastAPI("/ai/cover", result.getData());
+
+        } else if (result.getMessage() == "2") {
+                System.out.println(result.getData() + "@@@11111111111111111111111111111111111");
+                fastApiService.fetchDataFromFastAPI("/ai/cover", result.getData());
+
+        }
+        System.out.println(coverRegistDto.getSongPk());
+        for (UserPartDto userPartDto : coverRegistDto.getUserPartDtos()) {
+            System.out.println(userPartDto.getPartPk() +"@@@@@@@@@@@@@@" + userPartDto.getUserPk());
+        }
 
         return Response.of("OK","Make a Song 시작하기",null);
     }
