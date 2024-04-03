d6047f152ab537a44e22113868e040d5c79acb8c
LEESEOM <dltjdah25@naver.com>
2024-04-04 00:02:49 +0900

    feat : notification

--- FE/navi/src/widgets/LayoutHeader/ui/Alarm/Alarm.tsx
+++ FE/navi/src/widgets/LayoutHeader/ui/Alarm/Alarm.tsx
@@ -1,14 +1,57 @@
-import { Link } from 'react-router-dom';
+import { useNotiStore } from '@/shared/store';
 import { useUserStore } from '@/shared/store';
+import { useState } from 'react';
+import { baseApi } from '@/shared/api';
+import axios from 'axios';
 import css from './Alarm.module.css';
 
 export function Alarm() {
+  const noti = useNotiStore();
   const store = useUserStore();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleItemClick = async (id: number) => {
+    await axios.delete(`${baseApi}/alarms/delete/${id}`, {
+      withCredentials: true,
+    });
+    noti.getAlarms();
+  };
+
   if (store.isLogin) {
     return (
-      <Link to={'/noraebang'}>
-        <div className={css.root}></div>
-      </Link>
+      <div className={`dropdown dropdown-end dropdown-bottom ${css.dropdown}`}>
+        <div
+          tabIndex={0}
+          role="button"
+          className={`${css.root} ${noti.data.length === 0 ? css.root2 : ''}`}
+          onClick={noti.data.length > 0 ? toggleDropdown : undefined}
+        ></div>
+        {isOpen && (
+          <ul
+            tabIndex={0}
+            className={`bg-gray-00 menu dropdown-content z-[1] w-52 rounded-box bg-neutral-600 p-2 shadow ${css.dropdownContent}`}
+          >
+            {noti.data.map((notification) => (
+              <li
+                key={notification.id}
+                onClick={() => handleItemClick(notification.id)}
+              >
+                <a
+                  className={
+                    notification.alarmStatus === 'UNREAD' ? css.unread : ''
+                  }
+                >
+                  {notification.content}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     );
   }
   return null;
--- FE/navi/src/widgets/LayoutHeader/ui/LayoutHeader/LayoutHeader.tsx
+++ FE/navi/src/widgets/LayoutHeader/ui/LayoutHeader/LayoutHeader.tsx
@@ -2,6 +2,7 @@ import { Logo } from '../Logo/Logo';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { LogOutBtn } from '@/features/logout/';
 import { Alarm } from '../Alarm/Alarm';
+import SSEComponent from '@/features/notification/ui/Noti/Noti';
 import { LayoutHeaderUserImage } from '../LayoutHeaderUserImage/LayoutHeaderUserImage';
 import css from './LayoutHeader.module.css';
 import { Login } from '../Login/Login';
@@ -14,6 +15,7 @@ export function LayoutHeader() {
         <Searchbar />
       </div>
       <div className={css.right}>
+        <SSEComponent />
         <LogOutBtn />
         <Alarm />
         <LayoutHeaderUserImage />
