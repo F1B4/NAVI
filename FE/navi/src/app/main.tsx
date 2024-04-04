70267e721a35612124181d9e08bd99b4d3b142e1
LEESEOM <dltjdah25@naver.com>
2024-04-04 09:04:25 +0900

    fix : strictmode off

--- FE/navi/src/app/main.tsx
+++ FE/navi/src/app/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
@@ -8,8 +7,4 @@ import { appRouter } from './appRouter';
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <RouterProvider router={appRouter()} />
-  </StrictMode>,
-);
+root.render(<RouterProvider router={appRouter()} />);
